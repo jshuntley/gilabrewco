54f44faa06d35e8f83fee7d539352fe98142236a
jshuntley <josh@jshuntley.com>
2024-12-22 13:20:24 -0800

    added cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gilabrewing.com',
   output: 'server',
+  adapter: cloudflare(),
   integrations: [
     tailwind({
       applyBaseStyles: false
