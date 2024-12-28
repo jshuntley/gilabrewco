a99c34f48653b21ab96ba7f82bbf8d944128133c
jshuntley <josh@jshuntley.com>
2024-12-28 14:13:32 -0800

    changed typography & fixed theme changing issues

---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', '-apple-system', 'sans-serif'],
+        serif: ['Bitter', 'serif'],
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            h1: {
+              fontFamily: 'Bitter, serif',
+            },
+            h2: {
+              fontFamily: 'Bitter, serif',
+            },
+            h3: {
+              fontFamily: 'Bitter, serif',
+            },
+            h4: {
+              fontFamily: 'Bitter, serif',
+            },
+          },
+        },
+      },
+    },
+  },
+  // ... rest of your config
+}; 
